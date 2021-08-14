192cc5a3436374dc388a50bb46e6894ee75b024d
“anhek” <a.nhek@icloud.com>
2021-08-14 16:40:43 +0200

    updated: values reflecting the final design

--- src/features/backers/backersSlice.js
+++ src/features/backers/backersSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  value: 0,
+  value: 5007,
 };
 
 export const backersSlice = createSlice({
--- src/features/funds/fundsSlice.js
+++ src/features/funds/fundsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  value: 0,
+  value: 89914,
   goal: 100000,
 };
 
